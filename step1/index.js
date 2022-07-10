0f815506f41490e635d162d13d3b40ea876e0fdd
Meral Sager <mersa22@gmx.de>
2022-07-10 12:52:33 +0200

    Change Css z-index add more Card html

--- step1/index.js
+++ step1/index.js
@@ -1,10 +1,10 @@
 //Show answer
-alert("test");
+
 const button = document.querySelector('[data-js="button"]');
 const answer = document.querySelector('[data-js="answer"]');
 
 function onButtonClick() {
-  console.log("test");
+    console.log("test");
 }
 button.addEventListener("click", onButtonClick);
 
@@ -14,6 +14,16 @@ const bookmark = document.querySelector('[data-js="bookmark"]');
 let fill = document.querySelector('[data-js="bookmark"]');
 
 function onBookmark() {
-  console.log("test");
+    console.log("test");
 }
 bookmark.addEventListener("click", onBookmark);
+
+//const showAnswerButton = document.querySelector(
+// '[data-js="show-answer-button"]');
+// const answerContainer = document.querySelector('[data-js="answer-container"]');
+//  let isShown = false
+// showAnswerButton.addEventListener("click", () => {
+// isShown = !isShown;
+// answerContainer.classList.toggle("--hidden");
+
+// if (isShown) {showAnswerButton.innerText = "Hide Answer";console.log("isShown is now true");} else {showAnswerButton.innerText = "Show Answer";console.log("isShown is now false");}});
